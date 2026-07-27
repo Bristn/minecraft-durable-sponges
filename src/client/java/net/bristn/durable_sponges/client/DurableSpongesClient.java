b4433aab9b38d2e6458b3cefde7f41eec7bde49c
Bristn <Bristn@gmx.de>
2026-07-27 18:53:55 +0200

    Fix missing client initializer

---
+++ src/client/java/net/bristn/durable_sponges/client/DurableSpongesClient.java
@@ -0,0 +1,9 @@
+package net.bristn.durable_sponges.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DurableSpongesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
